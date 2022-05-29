1007ba16f43dc0e0141ff5970235bb401fe88d9c
Qualiaz <qualiaz@protonmail.com>
2022-05-29 17:22:14 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Restaurant Sushima
+
+Small project made as an assignment for theodinproject.com curriculum, using vanilla JS.
---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+"use strict";
+
+import "../styles/main.scss";
+import aboutUs from "./pages/about-us";
+import contact from "./pages/contact";
+import home from "./pages/home";
+import menu from "./pages/menu";
