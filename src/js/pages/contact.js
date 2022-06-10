32b47cc5629072334ea45ea09e10a34d775ec914
Qualiaz <qualiaz@protonmail.com>
2022-06-10 16:22:42 +0300

    initial contact

--- src/js/pages/contact.js
+++ src/js/pages/contact.js
@@ -1,12 +1,48 @@
+import restaurantOutdoor from "../../assets/contact/restaurant-outdoor.jpg";
+
 export default function contact() {
   const main = document.querySelector("#main");
+  main.textContent = undefined;
 
-  const mainContact = `<div class="main-home" id="homeContainer">
-                          <div class="main-home__cta">
-                            <p>Contact</p>
-                          </div>
-                        </div>
-`;
+  const contactContainer = document.createElement("div");
+  contactContainer.setAttribute("class", "main-contact");
+  main.appendChild(contactContainer);
 
-  main.innerHTML = mainContact;
+  // IMAGE
+  const restaurantOutsideImg = document.createElement("img");
+  restaurantOutsideImg.setAttribute("class", "main-contact__img");
+  restaurantOutsideImg.setAttribute("src", restaurantOutdoor);
+  contactContainer.appendChild(restaurantOutsideImg);
+
+  // CONTACT INFO
+  const contactInfo = document.createElement("div");
+  contactInfo.setAttribute("class", "main-contact__info");
+  contactContainer.appendChild(contactInfo);
+
+  const addressInfo = document.createElement("div");
+  contactInfo.appendChild(addressInfo);
+  const addressInfoTitle = document.createElement("h3");
+  addressInfoTitle.textContent = "Adresa:";
+  addressInfo.appendChild(addressInfoTitle);
+  const addressInfoText = document.createElement("p");
+  addressInfoText.textContent = "Bld. Iuliu Maniu 24, Bucuresti";
+  addressInfo.appendChild(addressInfoText);
+
+  const emailInfo = document.createElement("div");
+  contactInfo.appendChild(emailInfo);
+  const emailInfoTitle = document.createElement("h3");
+  emailInfoTitle.textContent = "Email:";
+  emailInfo.appendChild(emailInfoTitle);
+  const emailInfoText = document.createElement("p");
+  emailInfoText.textContent = "contact@sushima.com";
+  emailInfo.appendChild(emailInfoText);
+
+  const numberInfo = document.createElement("div");
+  contactInfo.appendChild(numberInfo);
+  const numberInfoTitle = document.createElement("h3");
+  numberInfoTitle.textContent = "Numar:";
+  numberInfo.appendChild(numberInfoTitle);
+  const numberInfoText = document.createElement("p");
+  numberInfoText.textContent = "0799483999";
+  numberInfo.appendChild(numberInfoText);
 }
