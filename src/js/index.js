815b87dfea09e71332f69797c848a22be9bdeabb
Qualiaz <qualiaz@protonmail.com>
2022-06-11 16:01:24 +0300

    added logo and shadows

--- src/js/index.js
+++ src/js/index.js
@@ -7,8 +7,22 @@ import contact from "./pages/contact";
 import home from "./pages/home";
 import menu from "./pages/menu";
 
-const hamburgerBtn = document.querySelector("#hamburgerBtn");
+import logoMain from "../assets/logo/logo-main.png";
+
 const header = document.querySelector("header");
+const footer = document.querySelector("footer");
+
+const hamburgerBtn = document.querySelector("#hamburgerBtn");
+const logoHeaderImg = document.createElement("img");
+logoHeaderImg.src = logoMain;
+
+const logoFooterImg = document.createElement("img");
+logoFooterImg.src = logoMain;
+
+header.insertAdjacentElement("afterbegin", logoHeaderImg);
+footer.insertAdjacentElement("afterbegin", logoFooterImg);
+logoHeaderImg.setAttribute("class", "logo");
+logoFooterImg.setAttribute("class", "logo");
 
 const homeBtn = document.querySelector("#homeBtn");
 const menuBtn = document.querySelector("#menuBtn");
